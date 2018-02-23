2e9749cd91bd4d805e924d1bd632dc1c42ae38ea
Eleutherado <emiliov432@gmail.com>
2018-02-22 21:55:23 -0500

    fix package.json

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const app = express();
 /*GET route handler, takes in a route and a callback that gets called when a
   request of that type arrives at that route*/
 app.get('/', (request, response) => {
-  res.send({ name: 'Emilio Vargas'});
+  response.send({ name: 'Emilio Vargas' });
 });
-
 
 const PORT = process.env.PORT || 5000;
 // Tells Node to listen on Port 5000
