6ced19c295d75471f308362e0d3e6a817116136b
Eleutherado <emiliov432@gmail.com>
2018-02-22 23:19:04 -0500

    began google oauth w passport

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const app = express();
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
-/*GET route handler, takes in a route and a callback that gets called when a
-  request of that type arrives at that route*/
-app.get('/', (request, response) => {
-  response.send({ name: 'Emilio Vargas' });
-});
+const app = express();
+const GOOGLE_CLIENT_ID = 370074158651-9imh8e9ptm81bl8molj26v24ms3vkrsu.apps.googleusercontent.com;
+const GOOGLE_CLIENT_SECRET = _Gxy_HL2kvSTvvbB9rBzZPSt;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: OUR_CALLBACK_URL
+    },
+    (accessToken, refreshToken, profile, callback) => {
+      User.findOrCreate({ googleId: profile.id }, (err, user) => {
+        return callback(err, user);
+      });
+    }
+  )
+);
 
 const PORT = process.env.PORT || 5000;
 // Tells Node to listen on Port 5000
