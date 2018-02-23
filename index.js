79a32a0e5accf8ee0f9ff365daa2345a815887e4
Eleutherado <emiliov432@gmail.com>
2018-02-22 23:40:02 -0500

    oops

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
-const GOOGLE_CLIENT_ID = 370074158651-9imh8e9ptm81bl8molj26v24ms3vkrsu.apps.googleusercontent.com;
-const GOOGLE_CLIENT_SECRET = _Gxy_HL2kvSTvvbB9rBzZPSt;
+
 
 passport.use(
   new GoogleStrategy(
